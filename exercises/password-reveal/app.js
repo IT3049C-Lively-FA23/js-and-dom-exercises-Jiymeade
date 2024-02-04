56164d4a0e3e5d2a7c47cc582ac2cac1638abaca
Jiymeade <jiymeade@gmail.com>
2024-02-03 19:45:56 -0500

    Password Finished with Cypress approvel

--- exercises/password-reveal/app.js
+++ exercises/password-reveal/app.js
@@ -1,7 +1,13 @@
 // 🤖: create 2 constants with references to the password input and the reveal button from the DOM
-
+const passwordInput = document.querySelector('#password');
+const togglePasswordButton = document.querySelector('#togglePassword');
 
 // 🤖: Create an Event Listener on the password reveal button
+togglePasswordButton.addEventListener('click', function () {
+	const currentType = passwordInput.getAttribute('type');
+	const newType = currentType === 'password' ? 'text' : 'password';
+		passwordInput.setAttribute('type', newType);
+});
 // 🤖: The event listener should toggle the password field attribute between `password` and `text` to change the visibility of the password field content.
 
 
