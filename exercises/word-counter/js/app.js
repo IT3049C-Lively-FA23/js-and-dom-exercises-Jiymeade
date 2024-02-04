0e2d9c66cca3394763b8a576a0621a4e6a30c347
Jiymeade <jiymeade@gmail.com>
2024-02-03 20:47:31 -0500

    Word-Counter passed by Cypress

--- exercises/word-counter/js/app.js
+++ exercises/word-counter/js/app.js
@@ -1,6 +1,14 @@
 // 🤖: create 2 constants with references to textarea input and the stats section from the DOM
-
+const textareaInput = document.getElementById('text');
+const statsSection =  document.getElementById('stat');
 
 // 🤖: Create an Event Listener on textarea input
+textareaInput.addEventListener('input', function() {
+  const text = textareaInput.value;
+  const words = text.trim().split(/\s+/).filter(word => word !== '').length;
+  const characters = text.length;
+  statsSection.textContent = `You've written ${words} words and ${characters} characters.`;
+});
+
 // 🤖: The event handler should update the stats section with the number of words and characters in the textarea input.
 // 🤖: You will need to utilize the split method to get the words count.
